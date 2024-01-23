d0a46c6b630f6358c96841edd4f4b958fb5bb76c
DANIAL <asaddanial@gmail.com>
2024-01-23 22:56:56 +0530

    DB connetivity

--- src/main/java/com/productservice/productservice/models/Product.java
+++ src/main/java/com/productservice/productservice/models/Product.java
@@ -1,15 +1,22 @@
 package com.productservice.productservice.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class Product {
+@Entity
+public class Product extends BaseModel{
     private String title;
-    private String desc;
+    private String description;
     private int price;
     private String image;
+    //Category is not a primitive attribute
+    //1 product belongs to 1 category
+    //1 category contains many products
+    @ManyToOne
     private Category category;
 
 }
